a11591e6caeaf772c294e8c54561b35184284ccb
Brookins Consulting <info@brookinsconsulting.com>
2015-06-09 17:18:00 -0500

    Updated: Bugfix: Removed menu.ini menu name typos and commented out menu.ini settings as not required or desired by default

--- settings/menu.ini.append.php
+++ settings/menu.ini.append.php
@@ -1,25 +1,38 @@
 <?php /* #?ini charset="utf8"?
 
-[NavigationPart]
-Part[ezcontenttreereportnavigationpart]=Folder Report
+# Note: The content of this menu.ini extension settings override
+#       is dissabled by default as not required. This is because
+#       the report module view provided by this extension is linked
+#       to using the 'ezpcollectivereportsadmin' extension (which does
+#       provide a top menu item for -all- our report extensions.
+#       Which would make the individual menu items for each report extension
+#       undesired as unessisary.
+#
+#       The following settings are provided as a referecne to users who
+#       need a admin top menu item but do not also use the 'ezpcollectivereportsadmin'
+#       extension (which would provide the admin top menu item for them). 
+#
 
-[TopAdminMenu]
-Tabs[]=contenttreereport
+#[NavigationPart]
+#Part[ezcontenttreereportnavigationpart]=Content Report
 
-[Topmenu_contenttreereport]
-NavigationPartIdentifier=ezcontenttreereportnavigationpart
-Name=Folder Report
-Tooltip=Folder Report
-URL[]
-URL[default]=contenttreereport/report
-Enabled[]
-Enabled[default]=true
-Enabled[browse]=false
-Enabled[edit]=false
-Shown[]
-Shown[default]=true
-Shown[edit]=false
-Shown[navigation]=true
-Shown[browse]=true
+#[TopAdminMenu]
+#Tabs[]=contenttreereport
+
+#[Topmenu_contenttreereport]
+#NavigationPartIdentifier=ezcontenttreereportnavigationpart
+#Name=Content Report
+#Tooltip=Content Report
+#URL[]
+#URL[default]=contenttreereport/report
+#Enabled[]
+#Enabled[default]=true
+#Enabled[browse]=false
+#Enabled[edit]=false
+#Shown[]
+#Shown[default]=true
+#Shown[edit]=false
+#Shown[navigation]=true
+#Shown[browse]=true
 
 */ ?>
